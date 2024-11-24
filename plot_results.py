3e51d0b7ef324ef1023813eb7cae3c0c8390b1b2
tbsalvis <tina.salvisberg@stud.hslu.ch>
2024-11-24 21:01:21 +0100

    plot_results taking result folder as an argument

--- plot_results.py
+++ plot_results.py
@@ -1,17 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
+import pandas as pd
 
 
-def main():
+def main(result_folder):
     # todo: load the "results.csv" file from the mia-results directory
     # todo: read the data into a list
     # todo: plot the Dice coefficients per label (i.e. white matter, gray matter, hippocampus, amygdala, thalamus)
     #  in a boxplot
+    file_path = f'mia-result/{result_folder}'
+    df = pd.read_csv(file_path)
+
+    print(f'Your folder is: {result_folder}\n')
+    print(df.head())
 
     # alternative: instead of manually loading/reading the csv file you could also use the pandas package
     # but you will need to install it first ('pip install pandas') and import it to this file ('import pandas as pd')
-    pass  # pass is just a placeholder if there is no other code
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Plotting the results.")
+    parser.add_argument("result_folder",
+                        type=str,
+                        help="The folder name inside mia-result that you want the metrics plotted from.")
+    args = parser.parse_args()
+    main(args.result_folder)
