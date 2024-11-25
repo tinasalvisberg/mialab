582ef209202e68f3becc894677fec17443183e60
tbsalvis <tina.salvisberg@stud.hslu.ch>
2024-11-25 09:04:58 +0100

    basic boxplot

--- plot_results.py
+++ plot_results.py
@@ -5,28 +5,26 @@ import pandas as pd
 
 
 def main(result_folder):
-    # TODO: plot the Dice coefficients per label (i.e. white matter, gray matter, hippocampus, amygdala, thalamus)
-    #  in a boxplot
     results_file_path = f'mia-result/{result_folder}/results.csv'
     summary_file_path = f'mia-result/{result_folder}/results_summary.csv'
     df = pd.read_csv(results_file_path, sep=';')
     summary = pd.read_csv(summary_file_path, sep=';')
 
     print(f'Results from: {result_folder}\n')
-    print(df.head())
-    print(summary.head())
 
-    amygdala = df[df['LABEL'] == 'Amygdala']
-    grey_matter = df[df['LABEL'] == 'GreyMatter']
+    labels = df['LABEL'].unique()
+    metrics = {'DICE': 'Dice score', 'HDRFDST': 'Hausdorff distance'}
 
-    print(amygdala.head())
-
-    plt.boxplot([amygdala['DICE'], grey_matter['DICE']])
-    plt.title('Dice score by class')
-    plt.ylabel('Dice')
-    plt.xticks([1, 2], ['Amygdala', 'Grey matter'])
-    plt.show()
-
+    for key, value in metrics.items():
+        plt.figure(figsize=(10, 6))
+        df.boxplot(column=key, by='LABEL', grid=True)
+        plt.title(f"{value} by label")
+        plt.suptitle('')  # Remove the automatic Pandas title
+        plt.ylabel(value)
+        plt.xlabel("Label")
+        # plt.xticks(rotation=45)  # Rotate x-axis labels if needed
+        plt.grid(True)
+        plt.show()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Plotting the results.")
