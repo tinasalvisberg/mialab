c51efeed9daf8640523db7f7f430d3d623b94cb3
tinasalvisberg <tina.salvisberg@stud.hslu.ch>
2024-12-15 10:05:34 +0100

    plotting all experiments

--- results_analysis.py
+++ results_analysis.py
@@ -3,6 +3,8 @@ import pandas as pd
 import warnings
 
 path_to_xlsx = f'results_analysis/results_consistency.xlsx'
+experiment_names = ['Baseline', 'Experiment 1', 'Experiment 2', 'Experiment 3', 'Experiment 4', 'Experiment 5',
+                    'Experiment 6', 'Experiment 7']
 
 # set sheet_name = None if you have more than two excel sheets -> creates a dict of dataframes
 res_a = pd.read_excel(path_to_xlsx, sheet_name=0)
@@ -14,22 +16,27 @@ dice_mean_b = res_b[(res_b['METRIC'] == 'DICE') & (res_b['STATISTIC'] == 'MEAN')
 hausd_mean_a = res_a[(res_a['METRIC'] == 'HDRFDST') & (res_a['STATISTIC'] == 'MEAN')]
 hausd_mean_b = res_b[(res_b['METRIC'] == 'HDRFDST') & (res_b['STATISTIC'] == 'MEAN')]
 
-plt.figure(figsize=(8, 6))
-plt.scatter(dice_mean_a["Baseline"], dice_mean_b["Baseline"], color='blue', label='Mean DICE Scores')
 
-# Adding diagonal for reference
-plt.plot([0.5, 0.85], [0.5, 0.85], color='red', linestyle='--', label='y=x (Perfect Agreement)')
+def compare_experiments(run_a: pd.DataFrame, run_b: pd.DataFrame, metric_name: str, experiment: str):
+    plt.figure(figsize=(8, 6))
+    plt.scatter(run_a[experiment], run_b[experiment], color='blue', label=f'Mean {metric_name} Scores')
 
-# Adding labels and title
-plt.xlabel("Experiment run A")
-plt.ylabel("Experiment run B")
-plt.title("Run A vs B baseline: Mean DICE Scores")
-plt.legend()
-plt.grid(True)
+    # Adding diagonal for reference
+    plt.plot([0.5, 0.85], [0.5, 0.85], color='red', linestyle='--', label='y=x (Perfect Agreement)')
+
+    # Adding labels and title
+    plt.xlabel("Experiment A")
+    plt.ylabel("Experiment B")
+    plt.title(f'Run A vs B {experiment}: Mean {metric_name} Scores')
+    plt.legend()
+    plt.grid(True)
+
+    # Annotating points with labels
+    for i, label in enumerate(run_a["LABEL"]):
+        plt.text(run_a[experiment].values[i] + 0.002, run_b[experiment].values[i], label, fontsize=9)
+    plt.show()
 
 
-# Annotating points with labels
-x_label = dice_mean_a['Baseline'].values
-for i, label in enumerate(dice_mean_a["LABEL"]):
-    plt.text(dice_mean_a["Baseline"].values[i] + 0.002, dice_mean_b["Baseline"].values[i], label, fontsize=9)
-plt.show()
+for exp in experiment_names:
+    compare_experiments(dice_mean_a, dice_mean_b, metric_name='Dice', experiment=exp)
+    compare_experiments(hausd_mean_a, hausd_mean_b, metric_name='Hausdorff distance', experiment=exp)
